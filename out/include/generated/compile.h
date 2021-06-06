462875a4085ec1f9fd0bb1c959a111c0508d655c
root <root@localhost.localdomain>
2021-06-06 16:29:41 +0800

    Adding Kernel Sources

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue May 11 12:38:39 PST 2021"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "kali"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
